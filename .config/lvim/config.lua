d8214db3ad9c7880b3782ba53b2576486e64d5be
Tom Saeger <tom.saeger@gmail.com>
2022-08-30 10:58:39 -0500

    reflow lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -21,13 +21,13 @@ lvim.leader = "space"
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+	name = "+Trouble",
+	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
 
 -- TODO: User Config for predefined plugins
@@ -40,15 +40,15 @@ lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "css",
-  "rust",
-  "yaml",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"css",
+	"rust",
+	"yaml",
 }
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
@@ -59,41 +59,48 @@ lvim.lsp.diagnostics.virtual_text = false
 -- formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
+	{ command = "black", filetypes = { "python" } },
+	-- { command = "isort", filetypes = { "python" } },
 }
 
 -- linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  { command = "flake8", filetypes = { "python" } },
-  { command = "shellcheck", extra_args = { "--severity", "warning" } },
-  { command = "codespell", },
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+	{ command = "flake8", filetypes = { "python" } },
+	{ command = "shellcheck", extra_args = { "--severity", "warning" } },
+	{ command = "codespell", },
 }
 
 -- Additional Plugins
 lvim.plugins = {
-  { "folke/trouble.nvim", cmd = "TroubleToggle", },
-  { "ggandor/lightspeed.nvim", }, -- https://github.com/ggandor/lightspeed.nvim
+	{ "folke/trouble.nvim", cmd = "TroubleToggle", },
+	{ "ggandor/lightspeed.nvim", }, -- https://github.com/ggandor/lightspeed.nvim
+	{ "simrat39/symbols-outline.nvim", },
 }
+-- TODO: add this to event callback
+pcall(function() require("symbols-outline").setup() end)
 
 -- Options toggles
 lvim.builtin.which_key.mappings["o"] = {
-  name = "+Options",
-  c = { "<cmd>lua vim.opt.cursorline = not vim.opt.cursorline:get()<cr>", "Toggle cursorline" },
-  h = { "<cmd>lua vim.opt.hlsearch = not vim.opt.hlsearch:get()<cr>", "Toggle hlsearch" },
-  l = { "<cmd>lua vim.opt.list = not vim.opt.list:get()<cr>", "Toggle list" },
-  n = { "<cmd>lua vim.opt.number = not vim.opt.number:get(); vim.opt.relativenumber = not vim.opt.relativenumber:get()<cr>",
-    "Toggle number" },
-  s = { "<cmd>lua vim.opt.spell = not vim.opt.spell:get()<cr>", "Toggle spell" },
-  w = { "<cmd>lua vim.opt.wrap = not vim.opt.wrap:get()<cr>", "Toggle wrap" },
+	name = "+Options",
+	c = { "<cmd>lua vim.opt.cursorline = not vim.opt.cursorline:get()<cr>", "Toggle cursorline" },
+	g = { "<cmd>Gitsigns toggle_signs<cr>", "Toggle git signs" },
+	h = { "<cmd>lua vim.opt.hlsearch = not vim.opt.hlsearch:get()<cr>", "Toggle hlsearch" },
+	l = { "<cmd>lua vim.opt.list = not vim.opt.list:get()<cr>", "Toggle list" },
+	n = { "<cmd>lua vim.opt.number = not vim.opt.number:get(); vim.opt.relativenumber = not vim.opt.relativenumber:get()<cr>",
+		"Toggle number" },
+	p = { "<cmd>lua vim.opt.paste = not vim.opt.paste:get()<cr>", "Toggle paste" },
+	s = { "<cmd>lua vim.opt.spell = not vim.opt.spell:get()<cr>", "Toggle spell" },
+	w = { "<cmd>lua vim.opt.wrap = not vim.opt.wrap:get()<cr>", "Toggle wrap" },
+	-- ? = toggle colorcolumn=80
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "zsh",
-  callback = function()
-    -- let treesitter use bash highlight for zsh files as well
-    require("nvim-treesitter.highlight").attach(0, "bash")
-  end,
+	pattern = "zsh",
+	callback = function()
+		-- let treesitter use bash highlight for zsh files as well
+		require("nvim-treesitter.highlight").attach(0, "bash")
+	end,
 })
