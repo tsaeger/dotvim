02802368c941644f63f50a474cdce36bb8e6f24a
Tom Saeger <tom.saeger@gmail.com>
2022-10-10 09:14:05 -0500

    lvim: stylua reflow

--- .config/lvim/lua/user/lsp.lua
+++ .config/lvim/lua/user/lsp.lua
@@ -2,19 +2,19 @@
 lvim.lsp.diagnostics.virtual_text = false
 
 -- formatters
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
   { command = "black", filetypes = { "python" } },
   { command = "isort", filetypes = { "python" } },
   { command = "stylua", filetypes = { "lua" } },
-}
+})
 
 -- linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
   { command = "flake8", filetypes = { "python" } },
   { command = "mypy", filetypes = { "python" } },
   { command = "shellcheck", extra_args = { "--severity", "warning" }, filetypes = { "bash" } },
   { command = "codespell" },
-}
+})
