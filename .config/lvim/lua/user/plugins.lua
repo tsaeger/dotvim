e8b120e1489c8a657bb538c6d50648c8220ae94f
Tom Saeger <tom.saeger@gmail.com>
2022-11-18 09:27:03 -0600

    feat(lvim): add nvim-surround plugin and deps

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -5,9 +5,29 @@ lvim.plugins = {
   { "gpanders/editorconfig.nvim" },
   { "jghauser/mkdir.nvim" },
   { "karb94/neoscroll.nvim" },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
   { "sainnhe/sonokai" },
   { "simrat39/rust-tools.nvim" },
   { "sindrets/diffview.nvim" },
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = {
+          insert = "<C-g>s",
+          insert_line = "<C-g>S",
+          normal = "ys",
+          normal_cur = "yss",
+          normal_line = "yS",
+          normal_cur_line = "ySS",
+          visual = "ys",
+          visual_line = "gS",
+          delete = "ds",
+          change = "cs",
+        },
+      })
+    end,
+  },
   {
     "j-hui/fidget.nvim",
     config = function()
