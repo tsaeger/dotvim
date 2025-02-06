a2974a635056e2157e4d41f37c212ed493f8e690
Tom Saeger <tom.saeger@gmail.com>
2025-02-06 11:04:53 -0600

    feat(nvim): add rust-analyzer
    Signed-off-by: Tom Saeger <tom.saeger@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -155,14 +155,9 @@ return {
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
-      -- rust_analyzer = {},
+      rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- ts_ls = {}, -- tsserver is deprecated
+      -- ts_ls = {},
       ruff = {},
       pylsp = {
         settings = {
