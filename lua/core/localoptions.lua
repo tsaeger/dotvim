e078a002fa4dec820aafcc1c3597f20ab1e9449f
Tom Saeger <tom.saeger@gmail.com>
2025-02-07 12:00:47 -0600

    feat(nvim): shada file support
    Signed-off-by: Tom Saeger <tom.saeger@gmail.com>

--- lua/core/localoptions.lua
+++ lua/core/localoptions.lua
@@ -3,3 +3,8 @@ vim.g.have_nerd_font = true
 vim.g.node_host_prog = '/Users/tsaeger/workspace/nix/node_modules/bin/neovim-node-host'
 vim.g.python3_host_prog = '/Users/tsaeger/.local/share/zinit/plugins/pyenv---pyenv/shims/python'
 vim.g.loaded_perl_provider = 0
+
+-- shadafile
+if vim.o.shadafile ~= nil then
+  vim.o.shadafile = vim.fn.stdpath 'cache' .. '/nvim2025_shada.dat'
+end
