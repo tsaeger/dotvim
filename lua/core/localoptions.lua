53066b332c49ba74d66abd2a2b681fa6fac26998
Tom Saeger <tom.saeger@gmail.com>
2025-02-11 17:57:13 -0600

    refactor(nvim): refactor local python settings
    Signed-off-by: Tom Saeger <tom.saeger@gmail.com>

--- lua/core/localoptions.lua
+++ lua/core/localoptions.lua
@@ -1,8 +1,18 @@
 vim.opt.guifont = 'MesloLGS Nerd Font:h20'
 vim.g.have_nerd_font = true
-vim.g.node_host_prog = '/Users/tsaeger/workspace/nix/node_modules/bin/neovim-node-host'
-vim.g.python3_host_prog = '/Users/tsaeger/.local/share/zinit/plugins/pyenv---pyenv/shims/python'
 vim.g.loaded_perl_provider = 0
+vim.g.node_host_prog = '/Users/tsaeger/workspace/nix/node_modules/bin/neovim-node-host'
+--[[
+## neovim python install
+
+```bash
+cargo install --git https://github.com/astral-sh/uv uv
+uv venv --python 3.12 nvim2025.venv
+source nvim2025.venv/bin/activate
+uv pip install pynvim
+```
+--]]
+vim.g.python3_host_prog = vim.g.myconfig.configdir .. '.venv/bin/python3'
 
 -- shadafile
 if vim.o.shadafile ~= nil then
