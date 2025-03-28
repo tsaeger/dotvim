9eacbb31130446765d0ceda70a0aa129cb8f7afe
Tom Saeger <tom.saeger@gmail.com>
2025-03-28 13:24:13 -0500

    refactor(nvim): lsp basedpyright disabled
    Disable basedpyright by default.
    
    Signed-off-by: Tom Saeger <tom.saeger@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,12 +6,7 @@ return {
     { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-    -- Useful status updates for LSP.
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim', opts = {} },
-
-    -- Allows extra capabilities provided by nvim-cmp
     'hrsh7th/cmp-nvim-lsp',
   },
   config = function()
@@ -169,7 +164,7 @@ return {
         -- },
       },
       -- gopls = {},
-      basedpyright = {},
+      basedpyright = { enabled = false },
       rust_analyzer = {
         -- prevent auto-config of rust_analyzer which interferes with rustaceanvim
         skip_autoconfigure = true,
