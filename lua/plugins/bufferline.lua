d0468550dee9390c2a20af81bf6189e134eebb61
Tom Saeger <tom.saeger@gmail.com>
2025-03-29 10:46:15 -0500

    feat(nvim): codespell'ify
    Add .codespellrc .codespell_exclude and
    fix all codespell(s)
    
    Signed-off-by: Tom Saeger <tom.saeger@gmail.com>

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,7 +8,7 @@ return {
     require('bufferline').setup {
       options = {
         mode = 'buffers', -- set to "tabs" to only show tabpages instead
-        themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+        themable = true, -- allows highlight groups to be overridden i.e. sets highlights as default
         numbers = 'none', -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
         close_command = 'Bdelete! %d', -- can be a string | function, see "Mouse actions"
         buffer_close_icon = '✗',
