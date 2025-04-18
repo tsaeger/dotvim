17f7e6a553cfe3093ec04214f0e56ad657d9e5b9
Tom Saeger <tom.saeger@gmail.com>
2025-04-18 14:43:11 -0500

    refactor(nvim): sync with kickstart-nvim 0.11 lsp settings
    Use same keymaps as 0.11:
    
    https://github.com/nvim-lua/kickstart.nvim/pull/1427
    
    Signed-off-by: Tom Saeger <tom.saeger@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,53 +36,74 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('<leader>cgd', require('telescope.builtin').lsp_definitions, '[C]ode [G]oto [D]efinition')
-
-        -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('<leader>cgr', require('telescope.builtin').lsp_references, '[C]ode [G]oto [R]eferences')
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>cgI', require('telescope.builtin').lsp_implementations, '[C]ode [G]oto [I]mplementation')
-
-        -- This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        map('<leader>cgD', vim.lsp.buf.declaration, '[C]ode [G]oto [D]eclaration')
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        map('<leader>cgt', require('telescope.builtin').lsp_type_definitions, '[C]ode [G]oto [T]ype Definition')
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        map('<leader>cds', require('telescope.builtin').lsp_document_symbols, '[C]ode [D]ocument [S]ymbols')
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        map('<leader>cws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[C]ode [W]orkspace [S]ymbols')
-
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
+        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
         map('<leader>cn', vim.lsp.buf.rename, '[C]ode Re[n]ame')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
+        map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+        -- Find references for the word under your cursor.
+        map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('<leader>cgr', require('telescope.builtin').lsp_references, '[C]ode [G]oto [R]eferences')
+
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
+        map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('<leader>cgI', require('telescope.builtin').lsp_implementations, '[C]ode [G]oto [I]mplementation')
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+        map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('<leader>cgd', require('telescope.builtin').lsp_definitions, '[C]ode [G]oto [D]efinition')
+
+        -- This is not Goto Definition, this is Goto Declaration.
+        --  For example, in C this would take you to the header.
+        map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('<leader>cgD', vim.lsp.buf.declaration, '[C]ode [G]oto [D]eclaration')
+
+        -- Fuzzy find all the symbols in your current document.
+        --  Symbols are things like variables, functions, types, etc.
+        map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+        map('<leader>cds', require('telescope.builtin').lsp_document_symbols, '[C]ode [D]ocument [S]ymbols')
+
+        -- Fuzzy find all the symbols in your current workspace.
+        --  Similar to document symbols, except searches over your entire project.
+        map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+        map('<leader>cws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[C]ode [W]orkspace [S]ymbols')
+
+        -- Jump to the type of the word under your cursor.
+        --  Useful when you're not sure what type a variable is and you want to see
+        --  the definition of its *type*, not where it was *defined*.
+        map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+        map('<leader>cgt', require('telescope.builtin').lsp_type_definitions, '[C]ode [G]oto [T]ype Definition')
+
+        -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+        ---@param client vim.lsp.Client
+        ---@param method vim.lsp.protocol.Method
+        ---@param bufnr? integer some lsp support methods only in specific files
+        ---@return boolean
+        local function client_supports_method(client, method, bufnr)
+          if vim.fn.has 'nvim-0.11' == 1 then
+            return client:supports_method(method, bufnr)
+          else
+            return client.supports_method(method, { bufnr = bufnr })
+          end
+        end
 
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
+        --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+        if
+          client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+        then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
@@ -106,7 +127,7 @@ return {
         end
 
         -- keymap for inlay hint toggle if supported by LSP
-        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
           map('<leader>ch', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
           end, '[C]ode Toggle Inlay [H]ints')
@@ -145,7 +166,7 @@ return {
         -- },
       },
       -- gopls = {},
-      basedpyright = { enabled = false },
+      basedpyright = { enabled = true },
       rust_analyzer = {
         -- prevent auto-config of rust_analyzer which interferes with rustaceanvim
         skip_autoconfigure = true,
